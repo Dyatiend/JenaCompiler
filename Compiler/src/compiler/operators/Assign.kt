3540aff975bba60f1f62b56a4d63d1b99c4ffb2e
Dyatiend <readysteel@ya.ru>
2022-10-29 14:55:50 +0300

    Исправлен оператор Assign

--- Compiler/src/compiler/operators/Assign.kt
+++ Compiler/src/compiler/operators/Assign.kt
@@ -1,121 +1,133 @@
-package compiler.operators;
+package compiler.operators
 
-import compiler.Operator;
-import compiler.Value;
-import util.CompilationResult;
-import util.DataType;
-import util.JenaUtil;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static util.JenaUtil.POAS_PREF;
-import static util.JenaUtil.VAR_PRED;
+import compiler.Operator
+import compiler.Value
+import compiler.values.PropertyValue
+import dictionaries.PropertiesDictionary
+import util.CompilationResult
+import util.DataType
+import util.JenaUtil
+import util.JenaUtil.POAS_PREF
+import util.JenaUtil.VAR_PRED
+import util.NamingManager
 
 /**
- * Присваивание TODO: пофиксить перезапись
+ * Присваивание
  */
-public class Assign extends BaseOperator {
+class Assign(args: List<Operator>) : BaseOperator(args) {
 
-    /**
-     * Шаблон правила присваивания значения свойства
-     */
-    private static final String PROPERTY_ASSIGN_PATTERN = "[<ruleHead>->(<subjName> <propName> <value>)]";
-
-    // drop(0) потому, что первый операнд всегда DecisionTreeVarValue и состоит из одной строки
-    /**
-     * Шаблон правила присваивания значения переменной дерева мысли
-     */
-    private static final String DECISION_TREE_VAR_ASSIGN_PATTERN = "[<ruleHead>->drop(0)(<newObj> <varPred> <varName>)]";
-
-    /**
-     * Конструктор
-     * @param args Аргументы
-     */
-    public Assign(List<Operator> args) {
-        super(args);
+    override fun argsDataTypes(): List<List<DataType>> {
+        val result: MutableList<List<DataType>> = ArrayList()
+        result.add(listOf(DataType.Object, DataType.Property, DataType.Literal))
+        result.add(listOf(DataType.DecisionTreeVar, DataType.Object))
+        return result
     }
 
-    @Override
-    public List<List<DataType>> argsDataTypes() {
-        List<List<DataType>> result = new ArrayList<>();
-
-        result.add(List.of(DataType.OBJECT, DataType.PROPERTY, DataType.LITERAL));
-        result.add(List.of(DataType.DECISION_TREE_VAR, DataType.OBJECT));
-
-        return result;
+    override fun resultDataType(): DataType? {
+        return null
     }
 
-    @Override
-    public DataType resultDataType() {
-        return null;
-    }
+    override fun compile(): List<CompilationResult> {
+        val result: MutableList<CompilationResult> = ArrayList()
 
-    @Override
-    public CompilationResult compile() {
-        // Объявляем переменные
-        String value = "";
-        String ruleHead = "";
-        String completedRules = "";
-
-        if(args().size() == 3) {
+        if (args().size == 3) {
             // Получаем аргументы
-            Operator arg0 = arg(0);
-            Operator arg1 = arg(1);
-            Operator arg2 = arg(2);
+            val arg0 = arg(0)
+            val arg1 = arg(1)
+            val arg2 = arg(2)
+
+            // Проверям, что свойство не статическое
+            require(PropertiesDictionary.isStatic((arg1 as PropertyValue).value)) {
+                "Свойство не должно быть статическим"
+            }
+
+            // Проверяем, что присваиваемое значение статическое
+            require(arg2 is Value || arg2 is GetPropertyValue) {
+                "Нельзя присвоить динамическое значение"
+            }
 
             // Компилируем аргументы
-            CompilationResult compiledArg0 = arg0.compile();
-            CompilationResult compiledArg1 = arg1.compile();
-            CompilationResult compiledArg2 = arg2.compile();
+            val compiledArg0 = arg0.compile()
+            val compiledArg1 = arg1.compile()
+            val compiledArg2 = arg2.compile()
 
-            // Генерируем правило
-            String operandsRuleHead = compiledArg0.ruleHead() + compiledArg1.ruleHead() + compiledArg2.ruleHead();
+            // Для всех результатов компиляции
+            compiledArg0.forEach { argRes0 ->
+                compiledArg1.forEach { argRes1 ->
+                    compiledArg2.forEach { argRes2 ->
+                        // Собираем правила для аргументов
+                        val head = argRes0.ruleHead + argRes1.ruleHead + argRes2.ruleHead
 
-            // Заполняем шаблон
-            String rule = PROPERTY_ASSIGN_PATTERN;
+                        // Заполняем шаблон
+                        var rule = PROPERTY_ASSIGN_PATTERN
+                        rule = rule.replace("<tmp0>", NamingManager.genVarName())
+                        rule = rule.replace("<tmp1>", NamingManager.genVarName())
 
-            rule = rule.replace("<ruleHead>", operandsRuleHead);
-            rule = rule.replace("<subjName>", compiledArg0.value());
-            rule = rule.replace("<propName>", compiledArg1.value());
-            rule = rule.replace("<value>", compiledArg2.value());
+                        rule = rule.replace("<ruleHead>", head)
+                        rule = rule.replace("<subjName>", argRes0.value)
+                        rule = rule.replace("<propName>", argRes1.value)
+                        rule = rule.replace("<value>", argRes2.value)
 
-            // Добавляем правило в набор правил
-            completedRules = compiledArg0.completedRules() +
-                    compiledArg1.completedRules() +
-                    compiledArg2.completedRules() +
-                    rule;
-        }
-        else {
+                        // Добавляем правило в набор правил
+                        val completedRules = argRes0.completedRules +
+                                argRes1.completedRules +
+                                argRes2.completedRules +
+                                rule
+
+                        // Добавляем в результат
+                        result.add(CompilationResult("", "", completedRules))
+                    }
+                }
+            }
+        } else {
             // Получаем аргументы
-            Operator arg0 = arg(0);
-            Operator arg1 = arg(1);
+            val arg0 = arg(0)
+            val arg1 = arg(1)
 
-            // FIXME?: сейчас переменную можно получить только из value, если это изменится, тогда придется фиксить
             // Получаем имя переменной
-            String varName = ((Value) arg0).value();
+            val varName = (arg0 as Value).value
 
             // Компилируем аргументы
-            CompilationResult compiledArg0 = arg0.compile();
-            CompilationResult compiledArg1 = arg1.compile();
+            val compiledArg0 = arg0.compile()
+            val compiledArg1 = arg1.compile()
 
-            // Генерируем правило
-            String operandsRuleHead = compiledArg0.ruleHead() + compiledArg1.ruleHead();
+            // Для всех результатов компиляции
+            compiledArg0.forEach { argRes0 ->
+                compiledArg1.forEach { argRes1 ->
+                    // Собираем правила для аргументов
+                    val head = argRes0.ruleHead + argRes1.ruleHead
 
-            // Заполняем шаблон
-            String rule = DECISION_TREE_VAR_ASSIGN_PATTERN;
+                    // Заполняем шаблон
+                    var rule = DECISION_TREE_VAR_ASSIGN_PATTERN
+                    rule = rule.replace("<ruleHead>", head)
+                    rule = rule.replace("<newObj>", argRes1.value)
+                    rule = rule.replace("<varPred>", JenaUtil.genLink(POAS_PREF, VAR_PRED))
+                    rule = rule.replace("<varName>", varName)
 
-            rule = rule.replace("<ruleHead>", operandsRuleHead);
-            rule = rule.replace("<newObj>", compiledArg1.value());
-            rule = rule.replace("<varPred>", JenaUtil.genLink(POAS_PREF, VAR_PRED));
-            rule = rule.replace("<varName>", varName);
+                    // Добавляем правило в набор правил
+                    val completedRules = argRes0.completedRules +
+                            argRes1.completedRules +
+                            rule
 
-            // Добавляем правило в набор правил
-            completedRules = compiledArg0.completedRules() +
-                    compiledArg1.completedRules() +
-                    rule;
+                    // Добавляем в результат
+                    result.add(CompilationResult("", "", completedRules))
+                }
+            }
         }
 
-        return new CompilationResult(value, ruleHead, completedRules);
+        return result
+    }
+
+    companion object {
+
+        /**
+         * Шаблон правила присваивания значения свойства
+         */
+        private const val PROPERTY_ASSIGN_PATTERN = "[\n(<tmp0> <propName> <tmp1>)\n<ruleHead>\nequal(<subjName>, <tmp0>)\n->\ndrop(0)\n(<subjName> <propName> <value>)\n]\n"
+
+        /**
+         * Шаблон правила присваивания значения переменной дерева мысли
+         */
+        private const val DECISION_TREE_VAR_ASSIGN_PATTERN = "[\n<ruleHead>\n->\ndrop(0)\n(<newObj> <varPred> <varName>)\n]\n"
     }
 }
