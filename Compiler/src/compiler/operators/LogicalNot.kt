80da7e96ed4a1e87819d8ea0f2c1635d8d735f61
Dyatiend <readysteel@ya.ru>
2022-10-30 10:32:12 +0300

    Исправлен оператор LogicalNot

--- Compiler/src/compiler/operators/LogicalNot.kt
+++ Compiler/src/compiler/operators/LogicalNot.kt
@@ -1,108 +1,23 @@
-package compiler.operators;
+package compiler.operators
 
-import compiler.Operator;
-import compiler.values.BooleanValue;
-import util.CompilationResult;
-import util.DataType;
-import util.JenaUtil;
-import util.NamingManager;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static util.JenaUtil.PAUSE_MARK;
+import compiler.Operator
+import util.CompilationResult
+import util.DataType
 
 /**
  * Логическое отрицание
  */
-public class LogicalNot extends BaseOperator {
+class LogicalNot(args: List<Operator>) : BaseOperator(args) {
 
-    /**
-     * Конструктор
-     * @param args Аргументы
-     */
-    public LogicalNot(List<Operator> args) {
-        super(args);
+    override fun argsDataTypes(): List<List<DataType>> {
+        return listOf(listOf(DataType.Boolean))
     }
 
-    @Override
-    public List<List<DataType>> argsDataTypes() {
-        List<List<DataType>> result = new ArrayList<>();
-
-        result.add(List.of(DataType.BOOLEAN));
-
-        return result;
+    override fun resultDataType(): DataType {
+        return DataType.Boolean
     }
 
-    @Override
-    public DataType resultDataType() {
-        return DataType.BOOLEAN;
-    }
-
-    @Override
-    public CompilationResult compile() {
-        // TODO: упрощение по законам де моргана?
-
-        // Объявляем переменные
-        String value = "";
-        String rulePart = "";
-        String completedRules = "";
-
-        String operRule = "";
-
-        // Получаем аргументы
-        Operator arg0 = arg(0);
-
-        // Компилируем аргументы
-        CompilationResult compiledArg0 = arg0.compile();
-
-        // Если оператор - булево значение
-        if (arg0 instanceof BooleanValue) {
-            // Добавляем выражение, равное значению
-            if(Boolean.parseBoolean(((BooleanValue) arg0).value())) {
-                operRule += JenaUtil.genEqualPrim("1", "1");
-            }
-            else {
-                operRule += JenaUtil.genEqualPrim("0", "1");
-            }
-        }
-
-        // Генерируем имена
-        String skolemName = NamingManager.genVarName();
-        String resFlagName = NamingManager.genPredName();
-
-        // Подставляем в сколем все объекты операторов
-        StringBuilder skolemArgs = new StringBuilder().append(skolemName);
-        for(String val : arg0.objectsUsedInRule()) {
-            skolemArgs.append(",");
-            skolemArgs.append(val);
-        }
-
-        // Генерируем правило
-        String rule = JenaUtil.genBooleanRule(operRule + compiledArg0.ruleHead(), skolemArgs.toString(), skolemName, resFlagName);
-
-        // Добавляем правило в набор завершенных правил
-        completedRules = compiledArg0.completedRules() + rule;
-
-        // Добавляем паузу
-        completedRules += PAUSE_MARK;
-
-        // FIXME?: придумать способ инициализации получше
-        // Инициализируем переменные в новом правиле
-        StringBuilder initPart = new StringBuilder();
-        for(String var : arg0.objectsUsedInRule()) {
-            initPart.append(JenaUtil.genTriple(var, NamingManager.genVarName(), NamingManager.genVarName()));
-        }
-        if(arg0.objectsUsedInRule().isEmpty()) {
-            initPart.append(JenaUtil.genTriple(NamingManager.genVarName(), NamingManager.genVarName(), NamingManager.genVarName()));
-        }
-
-        // Добавляем в правило проверку отсутствия флага
-        rulePart = initPart + JenaUtil.genMakeSkolemPrim(skolemArgs.toString()) +
-                JenaUtil.genNoValuePrim(skolemName, resFlagName);
-
-        usedObjects = new ArrayList<>(arg0.objectsUsedInRule());
-
-        return new CompilationResult(value, rulePart, completedRules);
+    override fun compile(): CompilationResult {
+        throw RuntimeException("Оператор LogicalNot должен быть удален при упрощении выражения")
     }
 }
