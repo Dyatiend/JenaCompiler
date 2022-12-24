b2ad53306ad2b68f67d993ac330caa05d9be1bdd
Dyatiend <readysteel@ya.ru>
2022-12-24 14:57:45 +0300

    Рефакторинг

--- Compiler/src/util/ComparisonResult.kt
+++ Compiler/src/util/ComparisonResult.kt
@@ -30,25 +30,23 @@ sealed class ComparisonResult {
      */
     object Undetermined : ComparisonResult()
 
-    override fun toString(): String = when (this) {
-        Equal -> "EQUAL"
-        NotEqual -> "NOT_EQUAL"
+    override fun toString() = when (this) {
         Greater -> "GREATER"
         Less -> "LESS"
+        Equal -> "EQUAL"
+        NotEqual -> "NOT_EQUAL"
         Undetermined -> "UNDETERMINED"
     }
-    
+
     companion object {
 
-        fun values(): Array<ComparisonResult> = arrayOf(Greater, Less, Equal, NotEqual, Undetermined)
-
-        fun valueOf(value: String): ComparisonResult = when (value) {
-                "GREATER" -> Greater
-                "LESS" -> Less
-                "EQUAL" -> Equal
-                "NOT_EQUAL"-> NotEqual
-                "UNDETERMINED" -> Undetermined
-                else -> throw IllegalArgumentException("No object util.ComparisonResult.$value")
+        fun valueOf(value: String) = when (value) {
+            "GREATER" -> Greater
+            "LESS" -> Less
+            "EQUAL" -> Equal
+            "NOT_EQUAL" -> NotEqual
+            "UNDETERMINED" -> Undetermined
+            else -> throw IllegalArgumentException("No object util.ComparisonResult.$value")
         }
     }
 }
