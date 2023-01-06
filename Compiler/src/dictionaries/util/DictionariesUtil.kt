0ed42660c163534a454e999890471940b167a2d7
Dyatiend <readysteel@ya.ru>
2023-01-06 10:15:06 +0300

    Рефакторинг

--- Compiler/src/dictionaries/util/DictionariesUtil.kt
+++ Compiler/src/dictionaries/util/DictionariesUtil.kt
@@ -5,26 +5,26 @@ import dictionaries.*
 /**
  * Разделитель столбцов в CSV файле словаря
  */
-const val COL_DELIMITER = '|'
+internal const val COLUMNS_SEPARATOR = '|'
 
 /**
- * Разделитель списков в ячейке CSV файла словаря
+ * Разделитель элементов списка в ячейке CSV файла словаря
  */
-const val LIST_DELIMITER = ';'
+internal const val LIST_ITEMS_SEPARATOR = ';'
 
 /**
  * Инициализирует все словари в правильном порядке
  */
 fun initAllDictionaries(
     classesDictionaryPath: String,
-    enumsDictionaryPath: String,
     decisionTreeVarsDictionaryPath: String,
+    enumsDictionaryPath: String,
     propertiesDictionaryPath: String,
     relationshipsDictionaryPath: String
 ) {
     ClassesDictionary.init(classesDictionaryPath) // Не зависит от других словарей
-    EnumsDictionary.init(enumsDictionaryPath) // Не зависит от других словарей
     DecisionTreeVarsDictionary.init(decisionTreeVarsDictionaryPath) // Зависит от словаря классов
+    EnumsDictionary.init(enumsDictionaryPath) // Не зависит от других словарей
     PropertiesDictionary.init(propertiesDictionaryPath) // Зависит от словаря классов и перечислений
     RelationshipsDictionary.init(relationshipsDictionaryPath) // Зависит от словаря классов
 }
