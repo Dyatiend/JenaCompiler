d486a6b970a5cf736ec6eded47f4ac3ec6c9b6d6
Dyatiend <readysteel@ya.ru>
2023-01-06 18:22:55 +0300

    Рефакторинг

--- Compiler/src/models/EnumModel.kt
+++ Compiler/src/models/EnumModel.kt
@@ -10,4 +10,20 @@ data class EnumModel(
     val name: String,
     val values: List<String>,
     val linerPredicate: String?
-)
+) {
+
+    init {
+        require(name.isNotBlank()) {
+            "Некорректное имя перечисления."
+        }
+        require(values.isNotEmpty()) {
+            "Перечисление $name не содержит значений."
+        }
+    }
+
+    /**
+     * Содержит ли перечисление указанное значение
+     * @param value Значение
+     */
+    fun containsValue(value: String) = values.contains(value)
+}
