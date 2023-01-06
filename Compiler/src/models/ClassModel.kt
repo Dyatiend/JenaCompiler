f231d37d355bee6c753e8e360ac4a468ad316095
Dyatiend <readysteel@ya.ru>
2023-01-06 18:18:23 +0300

    Рефакторинг

--- Compiler/src/models/ClassModel.kt
+++ Compiler/src/models/ClassModel.kt
@@ -5,12 +5,21 @@ import compiler.Operator
 /**
  * Модель класса в предметной области
  * @param name Имя класса
- * @param parent Имя родительского класса
+ * @param parent Модель класса родителя
  * @param calcExpr Выражение для вычисления
  * @see Operator
  */
 data class ClassModel(
     val name: String,
-    val parent: String?,
+    val parent: ClassModel?,
     val calcExpr: Operator?
-)
+) {
+
+    companion object {
+
+        /**
+         * Имя переменной в выражении для вычисления класса
+         */
+        const val CALC_EXPR_VAR_NAME = "obj"
+    }
+}
