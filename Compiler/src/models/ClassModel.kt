db36bf5b17e9a7ee6f752d6c496c23a5d1abfb6c
Dyatiend <readysteel@ya.ru>
2023-01-07 10:18:05 +0300

    Рефакторинг

--- Compiler/src/models/ClassModel.kt
+++ Compiler/src/models/ClassModel.kt
@@ -7,13 +7,13 @@ import util.DataType
 /**
  * Модель класса в предметной области
  * @param name Имя класса
- * @param parent Модель класса родителя
+ * @param parent Имя класса родителя
  * @param calcExpr Выражение для вычисления
  * @see Operator
  */
 data class ClassModel(
     val name: String,
-    val parent: ClassModel?,
+    val parent: String?,
     val calcExpr: Operator?
 ) {
 
@@ -24,8 +24,8 @@ data class ClassModel(
         require(!ClassesDictionary.exist(name)) {
             "Класс $name уже объявлен в словаре."
         }
-        require(parent == null || ClassesDictionary.exist(parent.name)) {
-            "Класс ${parent?.name} не объявлен в словаре."
+        require(parent == null || ClassesDictionary.exist(parent)) {
+            "Класс $parent не объявлен в словаре."
         }
         require(calcExpr == null || calcExpr.resultDataType == DataType.Boolean) {
             "Выражение для вычисления класса $name должно иметь тип Boolean, но имеет тип ${calcExpr?.resultDataType}."
