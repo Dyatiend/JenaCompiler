657ba7b8ce3e0db7cfc14858b8291385fd0a448d
Dyatiend <readysteel@ya.ru>
2023-01-07 08:30:47 +0300

    Рефакторинг

--- Compiler/src/models/DecisionTreeVarModel.kt
+++ Compiler/src/models/DecisionTreeVarModel.kt
@@ -1,5 +1,8 @@
 package models
 
+import dictionaries.ClassesDictionary
+import dictionaries.DecisionTreeVarsDictionary
+
 /**
  * Модель переменной дерева рассуждения
  * @param name Имя переменной
@@ -9,4 +12,17 @@ package models
 data class DecisionTreeVarModel(
     val name: String,
     val classModel: ClassModel
-)
+) {
+
+    init {
+        require(name.isNotBlank()) {
+            "Некорректное имя переменной."
+        }
+        require(!DecisionTreeVarsDictionary.exist(name)) {
+            "Переменная $name уже объявлена в словаре."
+        }
+        require(ClassesDictionary.exist(classModel.name)) {
+            "Класс ${classModel.name} не объявлен в словаре."
+        }
+    }
+}
